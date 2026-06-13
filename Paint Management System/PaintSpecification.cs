4016a821895e181e2a548e73ceaac2cdd25110e4
Guanyu Lin <limkoaniun@gmail.com>
2026-06-13 23:41:40 +1000

    Chore: format PaintSpecification.cs

--- Paint Management System/PaintSpecification.cs
+++ Paint Management System/PaintSpecification.cs
@@ -2,15 +2,15 @@ namespace Paint_Management_System;
 
 public class PaintSpecification
 {
-    public string Colour { get; set; }
-
-    public int SizeInLiters { get; set; }
-
     public PaintSpecification(string colour, int sizeInLiters)
     {
         Colour = colour;
         SizeInLiters = sizeInLiters;
     }
+
+    public string Colour { get; set; }
+
+    public int SizeInLiters { get; set; }
 
     public void DisplaySpecification()
     {
